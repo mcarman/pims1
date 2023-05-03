64801902e24b9d5d5143ecb885aee9ed25073158
mcarman <carman.md@live.com>
2023-05-03 10:09:08 -0600

    added dashy and smokeping to the stack

--- appdata/airsonic/config/rollback.sql
+++ appdata/airsonic/config/rollback.sql
@@ -2,13 +2,13 @@
 -- SQL to roll back currently unexecuted changes
 -- *********************************************************************
 -- Change Log: classpath:liquibase/db-changelog.xml
--- Ran at: 5/2/23, 8:41 AM
+-- Ran at: 5/3/23, 9:03 AM
 -- Against: SA@jdbc:hsqldb:file:/config/db/airsonic;hsqldb.tx=mvcc;sql.enforce_size=false;sql.char_literal=false;sql.nulls_first=false;sql.pad_space=false;hsqldb.defrag_limit=50;shutdown=true
 -- Liquibase version: 4.6.2
 -- *********************************************************************
 
 -- Lock Database
-UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '172.18.0.1 (172.18.0.1)', LOCKGRANTED = '2023-05-02 08:41:12.683' WHERE ID = 1 AND LOCKED = FALSE;
+UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '172.19.0.1 (172.19.0.1)', LOCKGRANTED = '2023-05-03 09:03:58.380' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Release Database Lock
 UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = FALSE, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
