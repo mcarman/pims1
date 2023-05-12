55d71cb9790f2c8aefe1ae477a39670692f8f354
mcarman <carman.md@live.com>
2023-05-12 09:03:43 -0600

    Clean up the notifiarr interface

--- appdata/airsonic/config/rollback.sql
+++ appdata/airsonic/config/rollback.sql
@@ -2,13 +2,13 @@
 -- SQL to roll back currently unexecuted changes
 -- *********************************************************************
 -- Change Log: classpath:liquibase/db-changelog.xml
--- Ran at: 5/8/23, 2:38 PM
+-- Ran at: 5/10/23, 11:12 AM
 -- Against: SA@jdbc:hsqldb:file:/config/db/airsonic;hsqldb.tx=mvcc;sql.enforce_size=false;sql.char_literal=false;sql.nulls_first=false;sql.pad_space=false;hsqldb.defrag_limit=50;shutdown=true
 -- Liquibase version: 4.6.2
 -- *********************************************************************
 
 -- Lock Database
-UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '172.25.0.1 (172.25.0.1)', LOCKGRANTED = '2023-05-08 14:38:18.568' WHERE ID = 1 AND LOCKED = FALSE;
+UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '172.22.0.1 (172.22.0.1)', LOCKGRANTED = '2023-05-10 11:12:14.368' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Release Database Lock
 UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = FALSE, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
