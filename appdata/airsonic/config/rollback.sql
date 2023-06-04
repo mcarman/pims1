931d6c8bdda073421da929a693a558ba8e17c623
mcarman <carman.md@live.com>
2023-06-04 17:19:05 -0600

    more samba prep.

--- appdata/airsonic/config/rollback.sql
+++ appdata/airsonic/config/rollback.sql
@@ -2,13 +2,13 @@
 -- SQL to roll back currently unexecuted changes
 -- *********************************************************************
 -- Change Log: classpath:liquibase/db-changelog.xml
--- Ran at: 6/1/23, 3:16 PM
+-- Ran at: 6/3/23, 3:16 PM
 -- Against: SA@jdbc:hsqldb:file:/config/db/airsonic;hsqldb.tx=mvcc;sql.enforce_size=false;sql.char_literal=false;sql.nulls_first=false;sql.pad_space=false;hsqldb.defrag_limit=50;shutdown=true
 -- Liquibase version: 4.6.2
 -- *********************************************************************
 
 -- Lock Database
-UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '3cd3e4eb1a75 (172.23.0.11)', LOCKGRANTED = '2023-06-01 15:16:08.068' WHERE ID = 1 AND LOCKED = FALSE;
+UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '3cd3e4eb1a75 (172.23.0.11)', LOCKGRANTED = '2023-06-03 15:16:51.814' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Release Database Lock
 UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = FALSE, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
